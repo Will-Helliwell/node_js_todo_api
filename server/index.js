6d3bb13f7fe89a8e2da25af380be0bae6130e3d6
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2022-05-16 22:15:40 +0100

    Add cors middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const port_number = 5000;
-
 app.listen(port_number, () => {
     console.log(`server has started on port ${port_number}`);
 });
