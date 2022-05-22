fb46ed002f5f99f7f9a8077842065e9c4c6848e1
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2022-05-22 16:25:43 +0100

    Post route to insert single todo. Uodate Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pern_todo
+
+## Learnings
+
+- pg
+- express + express.json()
+- cors
+- [exporting and importing](https://www.sitepoint.com/understanding-module-exports-exports-node-js/)
--- server/index.js
+++ server/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
-
 const pool = require('./db.js');
-
-const test = pool.query("SELECT * FROM todo");
-console.log(test);
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// ROUTES
+
+// Create a single to-do
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", 
+        [description]);
+        res.json(newTodo.rows[0]);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Get all to-dos
+
+// Get a single to-do
+
+// Update a to-do
+
+// Delete a to-do
+
 
 const port_number = 5000;
 app.listen(port_number, () => {
