c91c807144c24a89cb9e54e7ee446b0dd9b73c14
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2022-06-05 13:21:09 +0100

    Update route now takes description from body of request, returns a confirmation message

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", 
-        [description]);
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *",
+            [description]);
         res.json(newTodo.rows[0]);
     } catch (error) {
         console.log(error);
@@ -26,18 +26,18 @@ app.get("/todos", async (req, res) => {
     try {
         console.log('in get all route');
         const allTodos = await pool.query("SELECT * FROM todo");
-            res.json(allTodos.rows);
+        res.json(allTodos.rows);
     } catch (error) {
         console.log(error);
     }
-    });
+});
 
 // Get a single to-do
-app.get("/todos/:id", async(req, res) => {
+app.get("/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",
-        [id]);
+            [id]);
         res.json(todo.rows);
     } catch (err) {
         console.log(err);
@@ -45,12 +45,12 @@ app.get("/todos/:id", async(req, res) => {
 });
 
 // Update a to-do
-app.put("/todos/:id", async(req, res) => {
+app.put("/todos/:id", async (req, res) => {
     try {
-        const {id} = req.params;
-        const description = 'I must eat cheese';
-        const todo = pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *", [description, id]);
-        res.json(todo);
+        const { id } = req.params;
+        const { description } = req.body;
+        const todo = pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        res.json("Todo was updated!");
     } catch (err) {
         console.log(err);
     }
